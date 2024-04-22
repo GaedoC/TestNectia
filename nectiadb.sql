dfdd89f45fe8213077cce6abf2c50e0bd98b6c22
GaedoC <andres.aedoing@gmail.com>
2024-04-22 14:55:10 -0400

    Se agrega Script Base de datos MySQL para crear tabla users con datos de prueba

---
+++ nectiadb.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-04-2024 a las 20:51:23
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `nectiadb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(55) NOT NULL,
+  `email` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`) VALUES
+(1, 'John Doesn updateado', 'john@example.com'),
+(2, 'John Doe', 'john@example.com'),
+(3, 'John Does', 'john@example.com'),
+(4, 'John Doesn´t', 'john@example.com'),
+(5, 'John Doesn´t', 'john@example.com'),
+(6, 'John Doesn´t', 'john@example.com'),
+(7, 'John Doesn´ 1', 'john@example.com'),
+(8, 'John Doesn´ 2', 'john@example.com'),
+(9, 'John Doesn´t', 'john@example.com'),
+(10, 'John Doesn´t', 'john@example.com'),
+(11, 'John Doesn´t', 'john@example.com'),
+(12, 'John Doesn´t', 'john@example.com'),
+(13, 'John Doesn´t', 'john@example.com'),
+(14, 'John Doesn´t', 'john@example.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
